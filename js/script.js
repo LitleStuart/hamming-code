24c9f184779c974f1e35883955c6fa7b366c5ad7
LitleStuart <mukhorinvanga@gmail.com>
2021-10-20 11:20:49 +0500

    mistakes fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 var tabs = document.querySelector(".tabs");
 var instance = M.Tabs.init(tabs, {});
 function encode(result) {
+  result = result.toString();
+  if (result.length !== 4) {
+    alert("4 symbols only");
+    return;
+  } else {
+    for (let i = 0; i < result.length; i++) {
+      const char = result[i];
+      if (char !== "0" && char !== "1") {
+        alert("Only 0 or 1");
+        return;
+      }
+    }
+  }
   const digits = result.split("").map((el) => el * 1);
   result += (digits[0] + digits[2] + digits[3]) % 2;
   result += (digits[0] + digits[1] + digits[3]) % 2;
@@ -8,11 +21,25 @@ function encode(result) {
   document.getElementById("encodedStr").value = result;
   document.getElementById("strToDecode").value = result;
 }
+
 function decode(result) {
+  result = result.toString();
+  if (result.length !== 7) {
+    alert("7 symbols only");
+    return;
+  } else {
+    for (let i = 0; i < result.length; i++) {
+      const char = result[i];
+      if (char !== "0" && char !== "1") {
+        alert("Only 0 or 1");
+        return;
+      }
+    }
+  }
   const digits = result.split("").map((el) => el * 1);
-  if ((digits[4] + digits[0] + digits[2] + digits[3]) % 2 != 0) {
-    if ((digits[5] + digits[0] + digits[1] + digits[3]) % 2 != 0) {
-      if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 != 0) {
+  if ((digits[4] + digits[0] + digits[2] + digits[3]) % 2 !== 0) {
+    if ((digits[5] + digits[0] + digits[1] + digits[3]) % 2 !== 0) {
+      if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 !== 0) {
         digits[3] = (digits[3] + 1) % 2;
         alert(`Mistake found in ${4} digit`);
       } else {
@@ -20,7 +47,7 @@ function decode(result) {
         alert(`Mistake found in ${1} digit`);
       }
     } else {
-      if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 != 0) {
+      if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 !== 0) {
         digits[2] = (digits[2] + 1) % 2;
         alert(`Mistake found in ${3} digit`);
       } else {
@@ -28,15 +55,15 @@ function decode(result) {
         alert(`Mistake found in ${5} digit`);
       }
     }
-  } else if ((digits[5] + digits[0] + digits[1] + digits[3]) % 2 != 0) {
-    if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 != 0) {
+  } else if ((digits[5] + digits[0] + digits[1] + digits[3]) % 2 !== 0) {
+    if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 !== 0) {
       digits[1] = (digits[1] + 1) % 2;
       alert(`Mistake found in ${2} digit`);
     } else {
       digits[5] = (digits[5] + 1) % 2;
       alert(`Mistake found in ${6} digit`);
     }
-  } else if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 != 0) {
+  } else if ((digits[6] + digits[1] + digits[2] + digits[3]) % 2 !== 0) {
     digits[6] = (digits[6] + 1) % 2;
     alert(`Mistake found in ${7} digit`);
   } else {
